4fa4cf0ff73e1ba1c349721195d0559371b895f4
HarshaVardhan Project <harshaaffiliateprogram@gmail.com>
2026-05-25 19:57:04 +0100

    Update urls.py

--- futuretech/futuretech/urls.py
+++ futuretech/futuretech/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 
+
+def home(request):
+    return HttpResponse(
+        "<h1>AI Claude Traffic</h1><p>Your Django app is running.</p>",
+        content_type="text/html",
+    )
+
+
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
 ]
